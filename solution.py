eb8af6f68b2a2c80de3c56be1848be0ed60ed0d9
Zaitsev Maksim <108177495+Za1cevMaksim@users.noreply.github.com>
2023-04-03 15:32:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,21 +1,16 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import t
+from scipy.stats import laplace, t
 
 
 chat_id = 461694118 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    n = len(x)
+    n=len(x)
     x_bar = sum(x) / n
     s = np.sqrt(sum([(xi - x_bar) ** 2 for xi in x]) / (n - 1))
-    df = n - 1
-    t_value = t.ppf((1 + p) / 2, df)
-    margin_error = t_value * s / np.sqrt(n)
-    lower_bound = x_bar - margin_error
-    upper_bound = x_bar + margin_error
+    b = s / np.sqrt(2)
+    lower_bound = x_bar - laplace.ppf(1 - p / 2, scale=b) * np.sqrt(2) / n
+    upper_bound = x_bar + laplace.ppf(1 - p / 2, scale=b) * np.sqrt(2) / n
     return lower_bound, \
            upper_bound
