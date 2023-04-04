3a361af762c2cb085e07ff3124f61fe94f0d6414
Zaitsev Maksim <108177495+Za1cevMaksim@users.noreply.github.com>
2023-04-04 19:56:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,19 +6,20 @@ from scipy.stats import laplace, t
 chat_id = 461694118 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n=len(x)
+    n = len(x)
     x_bar = np.mean(x)
-    SE_x_bar = np.std(x, ddof=1) / np.sqrt(n)
-    time = 8
-    a_hat = 2 * x_bar / time ** 2
+    s = np.std(x, ddof=1)
+    delta = 0.5 - np.exp(1)
+    t_star = norm.ppf((1 + p) / 2)
+    q = np.sqrt(8) / (1 + np.sqrt(8) * x_bar)
+    d_q = -8 * np.sqrt(8) * (x_bar ** 2 + x_bar - 1) / ((1 + np.sqrt(8) * x_bar) ** 2)
+    v = (s ** 2) * (1 - 2 * delta / n) / n
+    d_v = (2 * delta / n ** 2) * (n * s ** 2 - (s ** 2) * (2 * n - 3))
+    sigma_q = np.sqrt((d_q ** 2) * v + (q ** 2) * d_v)
+    ME = t_star * sigma_q
+    lower_bound = abs(q - ME)
+    upper_bound = q + ME
 
-    SE_a_hat = 2 * SE_x_bar / time ** 2
-    alpha = 1-p  # significance level
-    df = n - 1  # degrees of freedom
-    t_crit = t.ppf(1 - alpha / 2, df)
-    CI = a_hat + np.array([-1, 1]) * t_crit * SE_a_hat
 
-
-
-    return CI[0], \
-           CI[1]
+    return lower_bound, \
+           upper_bound
