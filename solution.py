f2f4f020775c07d8f0366f009a3ad607fe9a9d7f
Zaitsev Maksim <108177495+Za1cevMaksim@users.noreply.github.com>
2023-04-04 19:25:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,10 +7,18 @@ chat_id = 461694118 # Ваш chat ID, не меняйте название пе�
 
 def solution(p: float, x: np.array) -> tuple:
     n=len(x)
-    x_bar = sum(x) / n
-    s = np.sqrt(sum([(xi - x_bar) ** 2 for xi in x]) / (n - 1))
-    b = s / np.sqrt(2)
-    lower_bound = x_bar - laplace.ppf(1 - p / 2, scale=b) * np.sqrt(2) / n
-    upper_bound = x_bar + laplace.ppf(1 - p / 2, scale=b) * np.sqrt(2) / n
-    return lower_bound, \
-           upper_bound
+    x_bar = np.mean(x)
+    SE_x_bar = np.std(x, ddof=1) / np.sqrt(n)
+    time = 8
+    a_hat = 2 * x_bar / time ** 2
+
+    SE_a_hat = 2 * SE_x_bar / time ** 2
+    alpha = 1-p  # significance level
+    df = n - 1  # degrees of freedom
+    t_crit = t.ppf(1 - alpha / 2, df)
+    CI = a_hat + np.array([-1, 1]) * t_crit * SE_a_hat
+
+
+
+    return CI[0], \
+           CI[1]
