8cb6517165b426d71d94e89ae90506daa8a9a35c
Zaitsev Maksim <108177495+Za1cevMaksim@users.noreply.github.com>
2023-04-07 00:33:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,25 +1,24 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import norm
+from scipy.stats import t
 
 
 chat_id = 461694118 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
+    n=len(x)
     x_bar = np.mean(x)
-    s = np.std(x, ddof=1)
-    delta = 0.5 - np.exp(1)
-    t_star = norm.ppf((1 + p) / 2)
-    q = np.sqrt(8) / (1 + np.sqrt(8) * x_bar)
-    d_q = -8 * np.sqrt(8) * (x_bar ** 2 + x_bar - 1) / ((1 + np.sqrt(8) * x_bar) ** 2)
-    v = (s ** 2) * (1 - 2 * delta / n) / n
-    d_v = (2 * delta / n ** 2) * (n * s ** 2 - (s ** 2) * (2 * n - 3))
-    sigma_q = np.sqrt((d_q ** 2) * v + (q ** 2) * d_v)
-    ME = t_star * sigma_q
-    lower_bound = abs(q - ME)
-    upper_bound = q + ME
+    SE_x_bar = np.std(x, ddof=1) / np.sqrt(n)
+    time = 8
+    a_hat = 2 * x_bar / time ** 2
 
+    SE_a_hat = 2 * SE_x_bar / time ** 2
+    alpha = 1-p
+    df = n - 1
+    t_crit = t.ppf(1 - alpha / 2, df)
+    er=t_crit * SE_a_hat
+    upper_bound = a_hat + er
+    lower_bound=a_hat-er
 
     return lower_bound, \
            upper_bound
