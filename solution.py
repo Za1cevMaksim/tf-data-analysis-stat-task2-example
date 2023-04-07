356380d1e7ab2edaf2c10d6e9ca192421b13b9eb
Zaitsev Maksim <108177495+Za1cevMaksim@users.noreply.github.com>
2023-04-07 23:34:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,22 +3,14 @@ import numpy as np
 from scipy.stats import t
 
 
-chat_id = 461694118 # Ваш chat ID, не меняйте название переменной
+chat_id = 461694118 
 
 def solution(p: float, x: np.array) -> tuple:
+    def solution(p: float, x: np.array) -> tuple:
     n=len(x)
-    x_bar = np.mean(x)
-    SE_x_bar = np.std(x, ddof=1) / np.sqrt(n)
-    time = 8
-    a_hat = 2 * x_bar / time ** 2
-
-    SE_a_hat = 2 * SE_x_bar / time ** 2
-    alpha = 1-p
-    df = n - 1
-    t_crit = t.ppf(1 - alpha / 2, df)
-    er=t_crit * SE_a_hat
-    upper_bound = a_hat + er
-    lower_bound=a_hat-er
-
+    t = 8
+    d1=(-np.log(p))/n
+    lower_bound=abs(2*(d1-1/2-min(x))/t**2)
+    upper_bound=abs(2*(-1/2-min(x))/t**2)
     return lower_bound, \
            upper_bound
