8314c12881d19e696e425f9240b2c5379a12aa7e
Zaitsev Maksim <108177495+Za1cevMaksim@users.noreply.github.com>
2023-04-07 23:35:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,6 @@ from scipy.stats import t
 chat_id = 461694118 
 
 def solution(p: float, x: np.array) -> tuple:
-    def solution(p: float, x: np.array) -> tuple:
     n=len(x)
     t = 8
     d1=(-np.log(p))/n
